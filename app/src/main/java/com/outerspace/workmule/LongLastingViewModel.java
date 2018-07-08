0316996c5554ea80c2ce1a7c80bc6fe0c9a81698
Luis Viruena <luis@motif.com>
2018-07-08 09:03:50 -0400

    This is a working version with a bug. The PageView does not pause the fragments when swipping them out, and that is the objective of the exercise

--- app/src/main/java/com/outerspace/workmule/LongLastingViewModel.java
+++ app/src/main/java/com/outerspace/workmule/LongLastingViewModel.java
@@ -2,22 +2,83 @@ package com.outerspace.workmule;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class LongLastingViewModel extends ViewModel {
 
     private MutableLiveData<String> currentMessage;
+    private MutableLiveData<Float> currentProgress;
 
     public LongLastingViewModel() {
-        this.currentMessage = new MutableLiveData<>();
+        currentMessage = new MutableLiveData<>();
+        currentProgress = new MutableLiveData<>();
     }
 
-    public MutableLiveData<String> getNext() {
+    public MutableLiveData<String> getCurrentMessage() {
+        return currentMessage;
+    }
+
+    public MutableLiveData<Float> getCurrentProgress() {
+        return currentProgress;
+    }
+
+    public void doLongLastingOperation() {
         String message = MessageStore.getInstance().getNext();
         currentMessage.setValue(message);
-        return  currentMessage;
+
+        MyLongLastingTask task = new MyLongLastingTask();
+        task.execute(2000, 10);
     }
 
-    public void reset() {
+    public void doResetOperation() {
         MessageStore.getInstance().reset();
     }
+
+    private class MyLongLastingTask extends AsyncTask<Integer, Float, Float> {
+
+        @Override
+        protected Float doInBackground(Integer... waitParams) {
+            if(waitParams.length == 2) {
+                Integer milliSeconds = waitParams[0];
+                Integer nPeriods = waitParams[1];
+                Float period = (float) milliSeconds / (float) nPeriods;
+                Float elapsed = 0.0f;
+
+                while(elapsed.intValue() < milliSeconds) {
+                    try {
+                        Thread.sleep(period.longValue());
+                        elapsed += period;
+                    } catch (InterruptedException e) {
+                        elapsed = (float) milliSeconds;
+                    }
+                    // currentProgress.setValue(elapsed);
+                    publishProgress(elapsed / (float) milliSeconds);
+                }
+            }
+            return 1.0f;
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... progressParams) {
+            if(progressParams.length == 1) {
+                currentProgress.setValue(progressParams[0]);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Float finalProgress) {
+            new CountDownTimer(100, 100) {
+                @Override public void onTick(long l) { }
+                @Override
+                public void onFinish() {
+                    currentProgress.setValue(null);
+                }
+            }.start();
+        }
+    }
+
 }
--- app/src/main/java/com/outerspace/workmule/TestViewModelFragment.java
+++ app/src/main/java/com/outerspace/workmule/TestViewModelFragment.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
@@ -19,11 +20,12 @@ import android.widget.Toast;
  */
 public class TestViewModelFragment extends Fragment {
 
-    private LongLastingViewModel model;
     private Context context;
-    private String channel_id;
-    private NotificationCompat.Builder builder;
-    private int notificationId = 927627;
+
+    private LongLastingViewModel model;
+    private String channelId;
+    private String notificationTitle;
+    private int notificationId;
 
     public TestViewModelFragment() { }     // Required empty public constructor
 
@@ -31,17 +33,16 @@ public class TestViewModelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-
-        context = container.getContext();
-        channel_id = getString(R.string.channel_id);
-        builder = new NotificationCompat.Builder(context, channel_id);
-
         View fragment;
         fragment = inflater.inflate(R.layout.fragment_test_view_model, container, false);
 
-        Button btnReset = (Button) fragment.findViewById(R.id.btn_reset);
-        Button btnRequest = (Button) fragment.findViewById(R.id.btn_request);
+        context = this.getContext();
 
+        channelId = getString(R.string.channel_id);
+        notificationTitle = getString(R.string.work_mule_notification_title);
+        notificationId = 927627;
+
+        Button btnReset = (Button) fragment.findViewById(R.id.btn_reset);
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,6 +50,7 @@ public class TestViewModelFragment extends Fragment {
             }
         });
 
+        Button btnRequest = (Button) fragment.findViewById(R.id.btn_request);
         btnRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,13 +58,37 @@ public class TestViewModelFragment extends Fragment {
             }
         });
 
+        /**
+         *  How does this work? :
+         *
+         *  The pieces:
+         *  LifecycleOwner - interface to denote a class has a lifecycle. typically an Activity or Fragment
+         *  LifecycleObserver - will receive lifecycle events from the lifecycleOwner
+         *
+         *      LifecycleOwner : fragment
+         *              ^
+         *              |
+         *              +---<- observes--- LifecycleObserver : MutableLiveData in the ViewModel.
+         *
+         *   The model is an object of LongLastingViewModel, a class extending ViewModel, and
+         *   therefore a lifecycleObserver.
+         *
+         *   This practice app uses ViewModelProviders, method of the ViewModelStore.
+         *   ViewModelProviders implements a factory pattern. when we call of, we get a
+         *   ViewModelProvider for the Fragment or the FragmentActivity that we specify.
+         *
+         *   a ViewModelProvider will retain ViewModel objects as long as the scope is valid
+         *   the get method retrieves the ViewModel or creates a new one. The provider can
+         *   hold one or more ViewModels identified by a key.
+         */
+
         model = ViewModelProviders.of(this).get(LongLastingViewModel.class);
 
-        final Observer<String> messageObserver = new Observer<String>() {
+        model.getCurrentMessage().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String message) {
-                // Toast.makeText(getActivity(), "Message:" +  message, Toast.LENGTH_SHORT).show();
-                String notificationTitle = getString(R.string.work_mule_notification_title);
+                NotificationCompat.Builder builder;
+                builder = new NotificationCompat.Builder(context, channelId);;
                 builder.setSmallIcon(R.drawable.ic_launcher_foreground)
                         .setContentTitle(notificationTitle)
                         .setContentText(message)
@@ -71,19 +97,46 @@ public class TestViewModelFragment extends Fragment {
                 NotificationManagerCompat manager = NotificationManagerCompat.from(context);
                 manager.notify(notificationId, builder.build());
             }
-        };
+        });
 
-        model.getNext().observe(this, messageObserver);
+        model.getCurrentProgress().observe(this, new Observer<Float>() {
+            @Override
+            public void onChanged(@Nullable Float progress) {
+                if(progress != null) {
+                    int iProgress = Float.valueOf(progress * 100.0f).intValue();
+
+                    NotificationCompat.Builder builder;
+                    builder = new NotificationCompat.Builder(context, channelId);;
+                    builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+                            .setContentTitle(notificationTitle)
+                            .setContentText(model.getCurrentMessage().getValue())
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setProgress(100, iProgress, false);
+
+                    NotificationManagerCompat
+                            .from(context)
+                            .notify(notificationId, builder.build());
+                } else {
+                    NotificationManagerCompat
+                            .from(context)
+                            .cancel(notificationId);
+                }
+            }
+        });
 
         return fragment;
+    }  // onCreate
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 
     public void onClickBtnReset(View view) {
-        model.reset();
+        model.doResetOperation();
     }
 
     public void onClickBtnRequest(View view) {
-        model.getNext();
+        model.doLongLastingOperation();
     }
-
 }
